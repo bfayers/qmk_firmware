6826464ad6bd7a5421cab5e870034c696d01751d
bfayers <bfayers@users.noreply.github.com>
2023-12-14 14:19:19 +0000

    Add cmd+spc on capslock

--- keyboards/keychron/k2_pro/iso/rgb/keymaps/bfayers/keymap.c
+++ keyboards/keychron/k2_pro/iso/rgb/keymaps/bfayers/keymap.c
@@ -31,6 +31,7 @@ tap_dance_action_t tap_dance_actions[] = {
 enum custom_keycodes {
     MAC_SSHOT = SAFE_RANGE,
     WIN_SSHOT,
+    CMD_SPC,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -43,6 +44,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           case WIN_SSHOT:
                if (record->event.pressed) {
                     SEND_STRING(SS_LGUI(SS_LSFT("S")));
+               }
+               break;
+          case CMD_SPC:
+               if (record->event.pressed) {
+                    SEND_STRING(SS_LCMD(" "));
                }
                break;
     }
@@ -62,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   MAC_SSHOT,  KC_DEL,   RGB_MOD,
      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_ENT,             KC_PGDN,
-     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_BSLS,                      KC_HOME,
+     CMD_SPC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_BSLS,                      KC_HOME,
      TD(TD_LSHIFT_CAPS),  KC_INT1,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,    KC_END,
      KC_LCTL,  KC_LOPT,  KC_LCMD,                               KC_SPC,                                 KC_RCMD,MO(MAC_FN),KC_RCTL,   KC_LEFT,  KC_DOWN,  KC_RGHT),
 
