a26332d32e37c2de3c587ec81721734eb4c8eb84
bfayers <bfayers@users.noreply.github.com>
2023-12-16 18:00:59 +0000

    create macro to cycle forwards through effects, skipping the empty one.

--- keyboards/keychron/k2_pro/iso/rgb/keymaps/bfayers/keymap.c
+++ keyboards/keychron/k2_pro/iso/rgb/keymaps/bfayers/keymap.c
@@ -39,7 +39,8 @@ tap_dance_action_t tap_dance_actions[] = {
 enum custom_keycodes {
     SSHOT = SAFE_RANGE,
     GUI_SPC,
-    OS_FN
+    OS_FN,
+    RGB_MODC
 };
 
 // This will be true when the switch is set to windows, and false when the switch is set to mac.
@@ -84,6 +85,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                }
                return false;
                break;
+          case RGB_MODC:
+               //Custom keycode to cycle forwards through rgb modes, but skip the empty effect.
+               if (record->event.pressed) {
+                    rgb_matrix_step();
+                    if ( rgb_matrix_get_mode() == RGB_MATRIX_CUSTOM_empty_effect ) {
+                         rgb_matrix_step();
+                    }
+               }
     }
     return true;
 }
@@ -113,8 +122,8 @@ void dynamic_macro_record_end_user(int8_t direction) {
 }
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+     //Handle the dynamic macro recording indicator(s).
      if (dynamic_recording) {
-          print("Matrix Function\n");
           if (DM_indicator_lit) {
                if (timer_elapsed(recording_timer) > 500) {
                     if (using_DM1) {
@@ -143,7 +152,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 //Definition of layers
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_iso_85(
-     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   SSHOT,  KC_DEL,   RGB_MOD,
+     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   SSHOT,  KC_DEL,     RGB_MODC,
      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_ENT,             KC_PGDN,
      GUI_SPC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_BSLS,                      KC_HOME,
@@ -153,7 +162,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [MAC_FN] = LAYOUT_iso_85(
      KC_TRNS,  KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_TRNS,  RGB_TOG,
      KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
-     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  DM_REC1,  DM_REC2,  KC_TRNS,            KC_TRNS,
+     RGB_TOG,  RGB_MODC,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  DM_REC1,  DM_REC2,  KC_TRNS,           KC_TRNS,
      KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  DM_PLY1,  DM_PLY2,                      KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
