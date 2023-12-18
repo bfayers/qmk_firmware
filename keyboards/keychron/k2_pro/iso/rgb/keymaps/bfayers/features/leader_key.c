7958ad2f8a510b531d080b6635a4c9bdb1bdf120
bfayers <bfayers@users.noreply.github.com>
2023-12-18 17:18:48 +0000

    ;

--- keyboards/keychron/k2_pro/iso/rgb/keymaps/bfayers/features/leader_key.c
+++ keyboards/keychron/k2_pro/iso/rgb/keymaps/bfayers/features/leader_key.c
@@ -5,6 +5,6 @@ void leader_end_user(void) {
     } else if (leader_sequence_two_keys(KC_F, KC_C)) {
         SEND_STRING("-ss start_time -to end_time");
     } else if (leader_sequence_two_keys(KC_F, KC_G)) {
-        SEND_STRING("-filter_complex \"[0:v] fps=25,scale=w=480:h=-1,split [a][b];[a] palettegen [p];[b][p] paletteuse\"")
+        SEND_STRING("-filter_complex \"[0:v] fps=25,scale=w=480:h=-1,split [a][b];[a] palettegen [p];[b][p] paletteuse\"");
     }
 }
